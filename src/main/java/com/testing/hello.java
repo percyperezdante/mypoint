74c3e6124730c138a69839f18b275f8f5874ca59
Percy <percyperezdante@gmail.com>
2019-09-10 15:20:32 +0100

    Update hello.java

--- src/main/java/com/testing/hello.java
+++ src/main/java/com/testing/hello.java
@@ -1,10 +1,11 @@
 package com.testing;
+import java.lang.Math;
 
 public class hello {
 
 	public static void main(String args[]){
 
-		System.out.println("Hello World, Maven");
+		System.out.println("Hello World, Maven " + Math.PI);
 
 	}
 
