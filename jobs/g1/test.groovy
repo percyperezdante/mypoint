3200e148f994650f7320a4953ad5eaa21c623f29
Percy <percyperezdante@gmail.com>
2021-02-09 23:18:24 +0000

    Update test.groovy

--- jobs/g1/test.groovy
+++ jobs/g1/test.groovy
@@ -4,3 +4,10 @@ job('example-1') {
         shell('echo Hello World!')
     }
 }
+
+// read file from workspace
+job('example-2') {
+    steps {
+        shell(readFileFromWorkspace('build.sh'))
+    }
+}
