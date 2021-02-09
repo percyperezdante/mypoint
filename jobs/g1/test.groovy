4c912d105e8c6af09257579e91b9f995f93404e3
Percy <percyperezdante@gmail.com>
2021-02-09 23:16:45 +0000

    Update test.groovy

--- jobs/g1/test.groovy
+++ jobs/g1/test.groovy
@@ -1,10 +1,6 @@
-pipeline {
-    agent  any 
-    stages {
-        stage('build') {
-            steps {
-                sh 'ls -ltr'
-            }
-        }
+// execute echo command
+job('example-1') {
+    steps {
+        shell('echo Hello World!')
     }
 }
