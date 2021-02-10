01e1e9b6f728540a17df403b1a4f70c4295a769e
Percy <percyperezdante@gmail.com>
2021-02-10 09:48:26 +0000

    Update test.groovy

--- jobs/g1/test.groovy
+++ jobs/g1/test.groovy
@@ -1,4 +1,7 @@
-pipelineJob('exampleWS') {
+folder('project2'){
+    displayName('project two')
+}
+pipelineJob('project2/exampleWS') {
     definition {
         cps {
             script(readFileFromWorkspace('jobs/g1/Jenkinsfile'))
