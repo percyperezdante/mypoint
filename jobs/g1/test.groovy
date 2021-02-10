50d1f17992a310b76b208be3acfdd3e55c012e0a
Percy <percyperezdante@gmail.com>
2021-02-10 00:08:01 +0000

    Update test.groovy

--- jobs/g1/test.groovy
+++ jobs/g1/test.groovy
@@ -1,8 +1,4 @@
-libraries {
-        // An identifier you pick for this library, to be used in the @Library annotation.
-        lib('mylib')
-}
-// execute echo command
+@Library('mylib')
 job('example-1') {
     steps {
         shell('echo Hello World!')
